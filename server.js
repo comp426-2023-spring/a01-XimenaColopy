f8f1a342858e0d6e3e292f5c9644c90a65ba6a75
XimenaColopy <43392764+XimenaColopy@users.noreply.github.com>
2023-01-17 17:01:18 -0500

    i think i did it

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 // Require http module
+var http = require('http');
 
 // Require fs module
+var fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
+var argv = require('minimist')(process.argv.slice(2));
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
+const PORT = argv.port || 3000
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
+var CONTENT = fs.readFile('./public/index.html', (err, data) => {
+    if (err) throw err;
+});
+
+//fs.readFile('./public/index.html',  function(err, data){
+
+    // Display the file content
+    //console.log(data);
+//});
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
@@ -27,9 +39,15 @@
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(CONTENT);
+});
 
-
-
+server.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
